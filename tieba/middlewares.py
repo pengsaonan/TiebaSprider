0a7db79f270b2e7eca311b66ae9b8af40589d38c
pansaolam <516533316@qq.com>
2018-11-16 11:25:35 +0800

    Revert "贴吧爬虫"
    This reverts commit 25c030830ababe77f0a24259050f7953ca9a7dff.

--- tieba/middlewares.py
+++
@@ -1,9 +0,0 @@
-import random
-from .settings import USER_AGENTS
-
-# 随机的User-Agent
-class RandomUserAgent(object):
-    def process_request(self, request, spider):
-        useragent = random.choice(USER_AGENTS)
-
-        request.headers.setdefault("User-Agent", useragent)
